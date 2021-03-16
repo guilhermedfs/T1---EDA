630616eac7fbdef6862137814047e9e646c03ca6
guilhermedfs <guilhermedfs11@gmail.com>
2021-03-16 11:06:04 -0300

    Menu estruturado

--- main.c
+++ main.c
@@ -1,28 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "menu.c"
 
 int main (){
-    int option;
-
-    printf("Indique a função a ser executada: \n\n");
-    printf("1 - Ler arquivo de dicionário\n");
-    printf("2 - Ler arquivo contendo texto de referência A (TRA)\n");
-    printf("3 - Ler arquivo contendo texto de referência B (TRB)\n");
-    printf("4 - Exibir BOWs de TRA e TRB na tela\n");
-    printf("5 - Mostrar similaridade entre os dois textos\n");
-    printf("6 - Sair\n\n");
-    printf("Insira o número indicando a função desejada: ");
-    scanf("%d", &option);
-
-    switch (option)
-    {
-    case 1:
-        break;
-    
-    default:
-        break;
-    }
-
-
+   menu();
     return 0;
 }
---
+++ menu.c
@@ -0,0 +1,18 @@
+int menu(){
+
+    int option;
+
+    printf("Indique a função a ser executada: \n\n");
+    printf("1 - Ler arquivo de dicionário\n");
+    printf("2 - Ler arquivo contendo texto de referência A (TRA)\n");
+    printf("3 - Ler arquivo contendo texto de referência B (TRB)\n");
+    printf("4 - Exibir BOWs de TRA e TRB na tela\n");
+    printf("5 - Mostrar similaridade entre os dois textos\n");
+    printf("6 - Sair\n\n");
+    printf("Insira o número indicando a função desejada: ");
+    scanf("%d", &option);
+
+    return option;
+    
+ }
+
