d3e54fea2414befb59423b019871e4c7ef2c2509
guilhermedfs <guilhermedfs11@gmail.com>
2021-03-16 12:03:24 -0300

    Adicionado campo de inserção do nome do dicionário

--- dicionario.c
+++ dicionario.c
@@ -1,14 +1,18 @@
 #include "dicionario.h"
 #include <stdio.h>
+#include <string.h>
 
 int dicionario(){
     FILE *arq;
 
-    char c, letra = '\n';
+    char c, letra = '\n', nome[100];
 
     int nPalavras = 0;
+    printf("\nInsira o nome do dicionário: ");
+    scanf("%s", nome);
+    strcat(nome, ".txt");
 
-    arq = fopen("dicionario2.txt", "r");
+    arq = fopen(nome, "r");
 
     while (fread (&c, sizeof(char), 1, arq))
     {
