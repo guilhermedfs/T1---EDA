15303bdca59908e43a5f99e19cd7ed883aaa8f38
victorear05 <78758172+victorear05@users.noreply.github.com>
2021-03-18 02:06:53 -0300

    complexidade linha à linha

--- main.c
+++ main.c
@@ -4,50 +4,52 @@
 #include "dicionario.h"
 #include "leituraTR.h"
 
-int baz;
+int baz;                                                                //O(1)
 
-int main (){
-    FILE *bowA = NULL;
-    FILE *bowB = NULL;
-    int op=1, n, *ContA, *ContB;
-    char nomeD[30], nomeBowA[30], nomeBowB[30];
-
-    while(op!=6)
+int main (){                                                                                
+    FILE *bowA = NULL;                                                  //O(1) 
+    FILE *bowB = NULL;                                                  //O(1)
+    int op=1, n, *ContA, *ContB;                                        //O(1)
+    char nomeD[30], nomeBowA[30], nomeBowB[30];                         //O(3)    
+                                                                        
+    while(op!=6)                                                        
     {
-         menu();
-         scanf("%d", &op);
-         switch (op)
-          {
-               case 1:
-                    dicionario(&n, nomeD);
+         menu();                                                        //O(9)
+         scanf("%d", &op);                                              //O(1)
+         switch (op)                                                    //O(n²) -> complexidade do switch (maior complexidade dentre as opções)
+          {                                                                
+               case 1:                                                  
+                    dicionario(&n, nomeD);                              //O(n) <- onde n é a uma estrura de repetição simples, dependendo do número de palavras de um arquivo
                     ContA = (int*) calloc(n, sizeof(int));
                     ContB = (int*) calloc(n, sizeof(int));
                     break;
-               case 2:
-                    bowA=leituraTR(nomeD, ContA, n, 1);
+               case 2:                                                  //O(n²) <- estrura de repetições aninhadas,e ambas dependentes do número de palavras do arquivo em todo caso
+                    bowA=leituraTR(nomeD, ContA, n, 1);                 
                     break;
-               case 3:
+               case 3:                                                  //O(n²) <- estrura de repetições aninhadas,e ambas dependentes do número de palavras do arquivo em todo caso
                     bowB=leituraTR(nomeD, ContB, n, 2);
                     break;
-               case 4:
-                    Exibir_Bows(bowA, nomeBowA, bowB, nomeBowB);
+               case 4:                                                  //O(n) <- onde n é a uma estrura de repetição simples, dependendo do número de palavras de um arquivo
+                    Exibir_Bows(bowA, nomeBowA, bowB, nomeBowB);    
                     break;
-               case 5:
+               case 5:                                                  
                     break;
-               case 6:
+               case 6:                                                  
 
                     break;
                default:
                     break;
-           }
-    }
-
-    if(baz == 0){
-   printf("%d\n", n);
+           }                            
+    }                                                                   //Complexidade do while: Ω(6*n²)  ->Melhor dos casos usuário utiliza cada funcionalidade uma vez,
+                                                                        //                       O(n*2n²) pior dos casos usuário ultiliza n vezes;
+    if(baz == 0){                                                       //O(1)
+   printf("%d\n", n);                                                   
     }
     
-     free (ContA);
-     free(ContB);
+     free (ContA);                                                      //O(1)
+     free(ContB);                                                       //O(1)
 
-    return 0;
-}
+    return 0;                                       
+}                                                                       //Complexidade O = O(n*(n²+10) + O(10) = O(n³) 
+                                                                        //Complexidade Ω = Ω(6*(n²+10) +  Ω(10) = Ω(n²)
+                                                                        //Complexidade Θ = Θ(z(n²+10) + Θ(10) = Θ(z*n²) -> onde 6 < z < n
